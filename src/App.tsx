f1406e2a2c61aa985d469ab39f7e6ff95f3c5086
Ava Silver <ava.silver@datadoghq.com>
2023-11-03 11:12:26 -0700

    update friends clocks

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function Clock({
 }: {
   name: string;
   tz: string;
-  twitter: string;
+  twitter?: string;
 }) {
   const timeContainer = useRef<HTMLSpanElement>(null);
 
@@ -29,10 +29,10 @@ function Clock({
     <div className="p-8">
       <div className="aspect-square border-black border-4 rounded-full grid place-items-center text-3xl relative">
         <span className="font-mono" ref={timeContainer}></span>
-        <img
+        {twitter && <img
           className="absolute rounded-full opacity-0 hover:opacity-100 transition-opacity duration-200"
           src={`https://unavatar.io/twitter/${twitter}`}
-        />
+        />}
       </div>
       <p className="text-center text-2xl mt-2">{name}</p>
     </div>
@@ -43,15 +43,16 @@ function App() {
   return (
     <div className="m-[15%]">
       <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
-        <Clock
-          name="Quartz"
-          tz="America/Los_Angeles"
-          twitter="piezo_electric"
-        />
-        <Clock name="Ava" tz="America/Los_Angeles" twitter="avaaa_42" />
+        <Clock name="Ava" tz="America/New_York" twitter="avapsilver" />
+        <Clock name="Vivi" tz="America/New_York" twitter="vivi_is_tired" />
+        <Clock name="Philo" tz="America/New_York" twitter="flaming_spork" />
+        <Clock name="Maxine" tz="America/New_York" twitter="prsmaticwolf" />
+        <Clock name="Tris" tz="America/New_York" />
+        <Clock name="Liz" tz="America/Los_Angeles" twitter="selectric401" />
+        <Clock name="Mia" tz="America/New_York" twitter="nkizz11" />
+        <Clock name="Ezgi" tz="America/New_York" twitter="ezgiburglar69" />
         <Clock name="Kay" tz="America/New_York" twitter="ffoorrggg" />
         <Clock name="Ula" tz="America/New_York" twitter="oh_jeez_bees" />
-        <Clock name="Aster" tz="Europe/Rome" twitter="Aster_ie" />
       </div>
     </div>
   );
