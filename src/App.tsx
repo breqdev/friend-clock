a3202b8b91fea57b2a8d72f3f59a4158a665434b
Brooke Chalmers <breq@breq.dev>
2023-11-09 22:46:57 -0500

    adjust grid

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function Clock({
   tz: string;
   github?: string;
 }) {
-  const timeContainer = useRef<HTMLSpanElement>(null);
+  const timeContainer = useRef<HTMLParagraphElement>(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -26,35 +26,31 @@ function Clock({
   });
 
   return (
-    <div className="p-8">
-      <div className="aspect-square border-black border-4 rounded-full grid place-items-center text-3xl relative">
-        <span className="font-mono" ref={timeContainer}></span>
-        {github && (
-          <img
-            className="absolute rounded-full opacity-0 hover:opacity-100 transition-opacity duration-200"
-            src={`https://github.com/${github}.png`}
-          />
-        )}
-      </div>
+    <div className="aspect-square border-black border-4 rounded-full flex flex-col justify-center align-center text-3xl relative">
+      <p className="text-center font-mono" ref={timeContainer}></p>
       <p className="text-center text-2xl mt-2">{name}</p>
+      {github && (
+        <img
+          className="absolute rounded-full opacity-0 hover:opacity-100 transition-opacity duration-200"
+          src={`https://github.com/${github}.png`}
+        />
+      )}
     </div>
   );
 }
 
 function App() {
   return (
-    <div className="m-[15%]">
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
-        <Clock name="Ava" tz="America/New_York" github="ava-silver" />
-        <Clock name="Vivi" tz="America/New_York" github="viv-codes" />
-        <Clock name="Charlotte" tz="America/Los_Angeles" github="q3w3e3" />
-        <Clock name="Wiggles" tz="America/Los_Angeles" />
-        <Clock name="Philo" tz="America/New_York" github="rpg4231" />
-        <Clock name="Maxine" tz="America/New_York" />
-        <Clock name="Tris" tz="America/New_York" github="an-empty-string" />
-        <Clock name="Liz" tz="America/Los_Angeles" github="selectric401" />
-        <Clock name="Mia" tz="America/New_York" github="nkizz" />
-      </div>
+    <div className="grid h-screen w-full grid-cols-[200px] lg:grid-cols-[200px,200px,200px] place-content-center gap-16">
+      <Clock name="Ava" tz="America/New_York" github="ava-silver" />
+      <Clock name="Vivi" tz="America/New_York" github="viv-codes" />
+      <Clock name="Maxine" tz="America/New_York" />
+      <Clock name="Mia" tz="America/New_York" github="nkizz" />
+      <Clock name="Philo" tz="America/New_York" github="rpg4231" />
+      <Clock name="Tris" tz="America/New_York" github="an-empty-string" />
+      <Clock name="Liz" tz="America/Los_Angeles" github="selectric401" />
+      <Clock name="Charlotte" tz="America/Los_Angeles" github="q3w3e3" />
+      <Clock name="Wiggles" tz="America/Los_Angeles" />
     </div>
   );
 }
